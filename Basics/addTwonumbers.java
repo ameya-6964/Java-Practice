7c72524b851fe275d04e84419ca59bca3c997f43
ameya-6964 <belvalkar7@gmail.com>
2022-07-23 20:07:16 +0530

    #23/07/2022

---
+++ Basics/addTwonumbers.java
@@ -0,0 +1,9 @@
+//This Java Program Prints Addition Of Two Numbers 
+public class addTwonumbers {
+    public static void main(String[] args) {
+        int a = 5;// Declaration
+        int b = 6;// Declaration
+        int result = a + b; // Declaration
+        System.out.println("The Result is : " + result); // Here I Have Used Concatination
+    }
+}
