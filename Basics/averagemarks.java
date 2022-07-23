6315749164dec659f8ff3b16e9fab08c02c80f61
ameya-6964 <belvalkar7@gmail.com>
2022-07-24 01:09:10 +0530

    #added Average program

---
+++ Basics/averagemarks.java
@@ -0,0 +1,11 @@
+//This java Program prints Average Of 3 Subjects 
+
+public class averagemarks {
+    public static void main(String[] args) {
+        int maths = 12;
+        int biology = 24;
+        int algebra = 50;
+        double avg = maths + biology + algebra / 3;
+        System.out.println("The Average Of Marks is : " + avg);
+    }
+}
