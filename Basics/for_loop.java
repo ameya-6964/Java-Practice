809bf53ebdb5c4886cd5197462d7c15b6f59f117
ameya-6964 <belvalkar7@gmail.com>
2022-07-28 22:17:02 +0530

    #loops

---
+++ Basics/for_loop.java
@@ -0,0 +1,8 @@
+public class for_loop {
+    public static void main(String[] args) {
+        int a = 0;
+        for (int i = 0; i <= 5; i++) {
+            System.out.println("numbers are:" + i);
+        }
+    }
+}
