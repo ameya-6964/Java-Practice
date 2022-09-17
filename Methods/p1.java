1302b72ab55797c3c1a52a52b718cbff66ff0c95
ameya-6964 <belvalkar7@gmail.com>
2022-09-17 18:05:56 +0530

    #added Methods

---
+++ Methods/p1.java
@@ -0,0 +1,17 @@
+class p1 {
+
+    public static void add() {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println(c);
+    }
+
+    public static void main(String[] args) {
+        add();
+
+    }
+
+}
+
+// output = 30
