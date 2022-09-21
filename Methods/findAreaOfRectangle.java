0457ca5e50aee5d74255dd39466b8571ced1e743
ameya-6964 <belvalkar7@gmail.com>
2022-09-21 22:10:38 +0530

    #rectangle Area Calculator

---
+++ Methods/findAreaOfRectangle.java
@@ -0,0 +1,18 @@
+// Create a Parameterised Method To Find Area Of Rectangle 
+
+public class findAreaOfRectangle {
+    public static int area(int a, int b) {
+        int res = a * b;
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int area1 = area(10, 20);
+        System.out.println(area1);
+    }
+}
+
+/*
+ * o/p
+ * 200
+ */
