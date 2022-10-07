9da90d30ba7d652d3a3df7772d784e5638dbaef9
ameya-6964 <belvalkar7@gmail.com>
2022-10-07 21:57:40 +0530

    #added

--- Coding Problems/printInteger.java
+++ Coding Problems/printInteger.java
@@ -1,14 +1,14 @@
 
 // Write a Java Program to Print Addition Of Two Integers (Input Should Be Taken By User)
-import java.util.Scanner;
-
 public class printInteger {
+
+    import java.util.Scanner;
     public static void main(String[] args) {
         System.out.println("Please Enter An Number");
         Scanner sc = new Scanner(input);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int res = a + b;
+        int a12 = sc.nextInt();
+        int b12 = sc.nextInt();
+        int res = a12 + b12;
         System.out.println("The Addition Of Two Numbers Is :" + res);
 
     }
